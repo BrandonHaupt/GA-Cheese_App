c168ac36234a2cc8a075b3b0c24ec288e123c8e0
Brandon Haupt <haupt.brandon@gmail.com>
2023-01-09 16:19:26 -0600

    update

--- cheese-frontend/src/pages/Show.js
+++ cheese-frontend/src/pages/Show.js
@@ -4,13 +4,13 @@ function Show() {
   const cheeses = useLoaderData();
 
   return (
-    <div className="cheeses">
+    <div className="cheese">
       <h1>{cheeses.name}</h1>
       <h2>{cheeses.title}</h2>
-      <img src={cheeses.image} alt={cheeses.name} />
+      <img className="cheese-img" src={cheeses.image} alt={cheeses.name} />
 
       <h2>Update {cheeses.name}</h2>
-      <Form action={`/update/${cheeses._id}`} method="post">
+      <Form action={`/update/${cheeses._id}`} method="post" className="form">
         <input type="input" name="name" placeholder="cheeses name" />
         <input type="input" name="image" placeholder="cheeses picture" />
         <input type="input" name="title" placeholder="cheeses title" />
@@ -18,8 +18,8 @@ function Show() {
       </Form>
 
       <h2>Delete Cheese</h2>
-      <Form action={`/delete/${cheeses._id}`} method="post">
-      <input type="submit" value={`Delete ${cheeses.name}`} />
+      <Form action={`/delete/${cheeses._id}`} method="post" className="form">
+        <input type="submit" value={`Delete ${cheeses.name}`} />
       </Form>
     </div>
   );
