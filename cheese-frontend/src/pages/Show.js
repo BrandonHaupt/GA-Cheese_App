8687564f9f0f7613b77225f5733ecdfd7e5da789
Brandon Haupt <haupt.brandon@gmail.com>
2023-01-09 16:11:26 -0600

    Finished Creating
    - finished creating app.

--- cheese-frontend/src/pages/Show.js
+++ cheese-frontend/src/pages/Show.js
@@ -1,8 +1,28 @@
+import { useLoaderData, Form } from "react-router-dom";
 
-export default function Show(){
-    return(
-        <>
-            <h1>Show</h1>
-        </>
-    )
+function Show() {
+  const cheeses = useLoaderData();
+
+  return (
+    <div className="cheeses">
+      <h1>{cheeses.name}</h1>
+      <h2>{cheeses.title}</h2>
+      <img src={cheeses.image} alt={cheeses.name} />
+
+      <h2>Update {cheeses.name}</h2>
+      <Form action={`/update/${cheeses._id}`} method="post">
+        <input type="input" name="name" placeholder="cheeses name" />
+        <input type="input" name="image" placeholder="cheeses picture" />
+        <input type="input" name="title" placeholder="cheeses title" />
+        <input type="submit" value={`Update ${cheeses.name}`} />
+      </Form>
+
+      <h2>Delete Cheese</h2>
+      <Form action={`/delete/${cheeses._id}`} method="post">
+      <input type="submit" value={`Delete ${cheeses.name}`} />
+      </Form>
+    </div>
+  );
 }
+
+export default Show;
--- cheese-frontend/src/router.js
+++ cheese-frontend/src/router.js
@@ -5,20 +5,19 @@ import {
 } from "react-router-dom"
 
 import App from "./App"
-import { cheeseLoader } from "./loader"
+import { cheeseLoader, cheesesLoader } from "./loader"
 import Index from "./pages/Index"
 import Show from "./pages/Show"
-import { createAction } from "./actions"
+import { createAction, updateAction, deleteAction } from "./actions"
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<App/>}>
             <Route path="" element={<Index/>} loader={cheeseLoader}/>
-            <Route path=":id" element={<Show/>}/>
+            <Route path=":id" element={<Show/>} loader={cheesesLoader}/>
             <Route path="create" action={createAction}/>
-            <Route path="create"/>
-            <Route path="update/:id"/>
-            <Route path="delete/:id"/>
+            <Route path="update/:id" action={updateAction}/>
+            <Route path="delete/:id" action={deleteAction}/>
         </Route>
     )
 )
